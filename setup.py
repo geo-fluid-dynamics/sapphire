ea74eae9bdd7f2539cabd5c6eb590eafd23e0068
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2018-11-27 11:51:45 +0100

    Remove fluff from setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,8 @@
 import setuptools
 
 
-with open("README.md") as f:
-
-    readme = f.read()
-
-with open("LICENSE.txt") as f:
-
-    license = f.read()
-
 setuptools.setup(
     name = "fempy",
     version = "0.1.0a0",
     packages = setuptools.find_packages(),
-    url = "https://github.com/alexanderzimmerman/fempy",
-    description = "Finite element models using Firedrake",
-    long_description = readme,
-    license = license,
-    author = "Alexander G. Zimmerman",
-    author_email = "zimmerman@aices.rwth-aachen.de",
 )
