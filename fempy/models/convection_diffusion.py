093b7abe583ed02b8c72f752cd7e0fb0ef36aea1
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2019-03-05 15:27:05 +0100

    Update convection-diffusion

--- fempy/models/convection_diffusion.py
+++ fempy/models/convection_diffusion.py
@@ -2,47 +2,59 @@
 import firedrake as fe
 import fempy.model
 
+
+dot, grad, div = fe.dot, fe.grad, fe.div
+    
+def variational_form_residual(model, solution):
+    
+    u = solution
+    
+    v = fe.TestFunction(solution.function_space())
+    
+    x = fe.SpatialCoordinate(solution.function_space().mesh())
+    
+    a = model.advection_velocity
+    
+    nu = model.kinematic_viscosity
+    
+    return v*dot(a, grad(u)) + dot(grad(v), nu*grad(u))
+    
+    
+def strong_form_residual(model, solution):
+    
+    x = fe.SpatialCoordinate(model.mesh)
+    
+    u = solution
+    
+    a = model.advection_velocity
+    
+    nu = model.kinematic_viscosity
+    
+    return dot(a, grad(u)) - div(nu*grad(u))
+
+
+def element(cell, degree):
+
+    return fe.FiniteElement("P", cell, degree)
+    
     
 class Model(fempy.model.Model):
     
     def __init__(self, 
             *args, 
-            mesh, element_degree, advection_velocity,
+            mesh,
+            element_degree,
+            advection_velocity,
             **kwargs):
         
         self.kinematic_viscosity = fe.Constant(1.)
         
         self.advection_velocity = advection_velocity(mesh)
     
-        element = fe.FiniteElement("P", mesh.ufl_cell(), element_degree)
-        
-        super().__init__(*args, mesh = mesh, element = element, **kwargs)
-        
-    def init_weak_form_residual(self):
-        
-        u, v = self.solution, fe.TestFunction(self.solution.function_space())
-        
-        x = fe.SpatialCoordinate(self.solution.function_space().mesh())
-        
-        a = self.advection_velocity
-        
-        nu = self.kinematic_viscosity
-        
-        dot, grad = fe.dot, fe.grad
-        
-        self.weak_form_residual = v*dot(a, grad(u)) + dot(grad(v), nu*grad(u))
-        
-    def strong_form_residual(self, solution):
-        
-        x = fe.SpatialCoordinate(self.mesh)
-        
-        u = solution
-        
-        a = self.advection_velocity
-        
-        nu = self.kinematic_viscosity
-        
-        dot, grad, div = fe.dot, fe.grad, fe.div
-        
-        return dot(a, grad(u)) - div(nu*grad(u))
+        super().__init__(*args,
+            mesh = mesh,
+            element = element(
+                cell = mesh.ufl_cell(), degree = element_degree),
+            variational_form_residual = variational_form_residual,
+            **kwargs)
         
--- tests/test__verify__convection_diffusion.py
+++ tests/test__verify__convection_diffusion.py
@@ -26,12 +26,16 @@ def test__verify_convergence_order_via_mms(
     
     fempy.mms.verify_spatial_order_of_accuracy(
         Model = fempy.models.convection_diffusion.Model,
+        weak_form_residual = \
+            fempy.models.convection_diffusion.variational_form_residual,
+        strong_form_residual = \
+            fempy.models.convection_diffusion.strong_form_residual,
+        manufactured_solution = manufactured_solution,
         meshes = [fe.UnitSquareMesh(n, n) for n in mesh_sizes],
         model_constructor_kwargs = {
             "quadrature_degree": quadrature_degree,
             "element_degree": 1,
             "advection_velocity": advection_velocity},
         parameters = {"kinematic_viscosity": 0.1},
-        manufactured_solution = manufactured_solution,
         expected_order = 2,
         tolerance = tolerance)
