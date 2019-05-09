873373a1d0d1142db045b12487f448daf5f3942b
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2019-05-09 15:10:58 +0200

    Remove firedrake from install_requires because setup cannot find it

--- setup.py
+++ setup.py
@@ -6,11 +6,8 @@ setuptools.setup(
     version = '0.4.3a0',
     packages = setuptools.find_packages(),
     install_requires = [
-        'firedrake',
         'matplotlib',
         'numpy',
         'scipy',
-        'vtk'],
-     dependency_links = [
-        'https://github.com/firedrakeproject/firedrake',]
+        'vtk']]
 )
