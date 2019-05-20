22e967823dd7e36e3323c3807abfa9732884a4b2
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2019-05-20 15:53:00 +0200

    Verify only second order accuracy both with automatic quadrature rule and second degree quadrature

--- tests/model_verification/test__convection_coupled_phasechange.py
+++ tests/model_verification/test__convection_coupled_phasechange.py
@@ -8,33 +8,41 @@ from sapphire.simulations import convection_coupled_phasechange as sim_module
 
 tempdir = sapphire.test.datadir
 
-def manufactured_solution(sim):
+pi, sin, exp, dot = fe.pi, fe.sin, fe.exp, fe.dot
 
-    pi, sin, cos, exp = fe.pi, fe.sin, fe.cos, fe.exp
+def manufactured_solution(mesh, time):
+
+    x = fe.SpatialCoordinate(mesh)
     
-    x = fe.SpatialCoordinate(sim.mesh)
+    ihat, jhat = sapphire.simulation.unit_vectors(mesh)
     
-    t = sim.time
+    t = time
     
-    t_f = fe.Constant(1.)
-    
-    ihat, jhat = sapphire.simulation.unit_vectors(sim.mesh)
-    
-    u = exp(t)*sin(2.*pi*x[0])*sin(pi*x[1])*ihat + \
-        exp(t)*sin(pi*x[0])*sin(2.*pi*x[1])*jhat
+    u = exp(t/2)*sin(2.*pi*x[0])*sin(pi*x[1])*ihat + \
+        exp(t/2)*sin(pi*x[0])*sin(2.*pi*x[1])*jhat
     
     p = -sin(pi*x[0])*sin(2.*pi*x[1])
     
-    T = 0.5*sin(2.*pi*x[0])*sin(pi*x[1])*(1. - exp(-t**2))
+    T = 0.5*sin(2.*pi*x[0])*sin(pi*x[1])*(1. - exp(-0.5*t**2))
     
     return p, u, T
-    
 
+
+def space_verification_solution(sim):
+
+    return manufactured_solution(mesh = sim.mesh, time = 1.)
+
+    
+def time_verification_solution(sim):
+
+    return manufactured_solution(mesh = sim.mesh, time = sim.time)
+
+    
 Gr = 3.6e5
 
-Pr = 5.
+Pr = 7.0
 
-Ste = 0.2
+Ste = 0.13
 
 rhos_over_rhol = 0.92
 
@@ -42,18 +50,12 @@ cs_over_cl = 0.50
 
 kappas_over_kappal = 3.8
 
-sigma = 1/16
+sigma = 0.1
 
-endtime = 0.5
-
-q = None
+endtime = 1.
 
 def test__verify__second_order_spatial_convergence__via_mms(
         tempdir,
-        sim_constructor_kwargs = {
-            "quadrature_degree": q,
-            "element_degree": 1,
-            "time_stencil_size": 3},
         parameters = {
             "grashof_number": Gr,
             "prandtl_number": Pr,
@@ -62,123 +64,33 @@ def test__verify__second_order_spatial_convergence__via_mms(
             "heat_capacity_solid_to_liquid_ratio": cs_over_cl,
             "thermal_conductivity_solid_to_liquid_ratio": kappas_over_kappal,
             "smoothing": sigma},
-        mesh_sizes = (2, 4, 8),
-        timestep_size = 1./64.,
-        tolerance = 0.2):
-    
-    rt = sim_constructor_kwargs["time_stencil_size"] - 1
-    
-    outpath = tempdir.join("{}/{}/".format(
-        __loader__.fullname,  sys._getframe().f_code.co_name))
-    
-    outpath.mkdir(parents = True, exist_ok = True)
-    
-    sapphire.mms.verify_spatial_order_of_accuracy(
-        sim_module = sim_module,
-        manufactured_solution = manufactured_solution,
-        sim_constructor_kwargs = sim_constructor_kwargs,
-        meshes = [fe.UnitSquareMesh(n, n) for n in mesh_sizes],
-        parameters = parameters,
-        expected_order = 2,
-        tolerance = tolerance,
-        timestep_size = timestep_size,
-        endtime = endtime,
-        outfile = open(outpath.join("convergence_rt{}_Deltat{}.csv".format(
-            rt, timestep_size))))
-        
-        
-def test__verify__second_order_temporal_convergence__via_mms(
-        tempdir,
-        sim_constructor_kwargs = {
-            "quadrature_degree": q,
-            "element_degree": 1,
-            "time_stencil_size": 3},
-        parameters = {
-            "grashof_number": Gr,
-            "prandtl_number": Pr,
-            "stefan_number": Ste,
-            "density_solid_to_liquid_ratio": rhos_over_rhol,
-            "heat_capacity_solid_to_liquid_ratio": cs_over_cl,
-            "thermal_conductivity_solid_to_liquid_ratio": kappas_over_kappal,
-            "smoothing": sigma},
-        meshsize = 24,
-        timestep_sizes = (1./3., 1./9., 1./27.),
-        tolerance = 0.3):
-    
-    rx = sim_constructor_kwargs["element_degree"] + 1
-    
-    mesh = fe.UnitSquareMesh(meshsize, meshsize)
-    
-    h = mesh.cell_sizes((0.,)*mesh.geometric_dimension())
-    
-    sapphire.mms.verify_temporal_order_of_accuracy(
-        sim_module = sim_module,
-        manufactured_solution = manufactured_solution,
-        sim_constructor_kwargs = sim_constructor_kwargs,
-        mesh = mesh,
-        parameters = parameters,
-        expected_order = 2,
-        tolerance = tolerance,
-        timestep_sizes = timestep_sizes,
-        endtime = endtime,
-        outfile = open(tempdir.join("{}/rt2_rx{}_h{}.csv".format(
-            outdir, rx, h))))
-
-        
-def test__verify__third_order_spatial_convergence__via_mms(
-        tempdir,
-        sim_constructor_kwargs = {
-            "quadrature_degree": q,
-            "element_degree": 2,
-            "time_stencil_size": 4},
-        parameters = {
-            "grashof_number": Gr,
-            "prandtl_number": Pr,
-            "stefan_number": Ste,
-            "density_solid_to_liquid_ratio": rhos_over_rhol,
-            "heat_capacity_solid_to_liquid_ratio": cs_over_cl,
-            "thermal_conductivity_solid_to_liquid_ratio": kappas_over_kappal,
-            "smoothing": sigma},
-        mesh_sizes = (4, 8, 16, 32),
-        timestep_size = 1/64,
-        tolerance = 0.2):
-    
-    tempdir_path = pathlib.Path(tempdir)
+        mesh_sizes = (8, 16, 32, 64),
+        tolerance = 0.1):
     
     testdir = "{}/{}/".format(
         __name__.replace(".", "/"), sys._getframe().f_code.co_name)
     
-    outdir_path = tempdir_path / testdir
+    outdir_path = pathlib.Path(tempdir) / testdir
     
     outdir_path.mkdir(parents = True, exist_ok = True) 
     
-    rt = sim_constructor_kwargs["time_stencil_size"] - 1
-    
-    filename = "rt{}_Deltat{}.csv".format(rt, timestep_size)
-    
-    outfile_path = outdir_path / filename
-    
-    with open(outfile_path, "w") as outfile:
+    with open(outdir_path / "convergence.csv", "w") as outfile:
         
         sapphire.mms.verify_spatial_order_of_accuracy(
             sim_module = sim_module,
-            manufactured_solution = manufactured_solution,
-            sim_constructor_kwargs = sim_constructor_kwargs,
+            manufactured_solution = space_verification_solution,
             meshes = [fe.UnitSquareMesh(n, n) for n in mesh_sizes],
             parameters = parameters,
-            expected_order = 3,
+            expected_order = 2,
             tolerance = tolerance,
-            timestep_size = timestep_size,
+            timestep_size = endtime,
             endtime = endtime,
             outfile = outfile)
             
-        
-def test__verify__third_order_temporal_convergence__via_mms(
+            
+def test__verify__second_order_spatial_convergence__low_quadrature__via_mms(
         tempdir,
-        sim_constructor_kwargs = {
-            "quadrature_degree": q,
-            "element_degree": 2,
-            "time_stencil_size": 4},
+        sim_constructor_kwargs = {"quadrature_degree": 2},
         parameters = {
             "grashof_number": Gr,
             "prandtl_number": Pr,
@@ -187,26 +99,99 @@ def test__verify__third_order_temporal_convergence__via_mms(
             "heat_capacity_solid_to_liquid_ratio": cs_over_cl,
             "thermal_conductivity_solid_to_liquid_ratio": kappas_over_kappal,
             "smoothing": sigma},
-        meshsize = 24,
-        timestep_sizes = (1./4., 1./8., 1./16.),
-        tolerance = 0.3):
+        mesh_sizes = (8, 16, 32, 64),
+        tolerance = 0.1):
     
-    rx = sim_constructor_kwargs["element_degree"] + 1
+    testdir = "{}/{}/".format(
+        __name__.replace(".", "/"), sys._getframe().f_code.co_name)
     
-    mesh = fe.UnitSquareMesh(meshsize, meshsize)
+    outdir_path = pathlib.Path(tempdir) / testdir
     
-    h = mesh.cell_sizes((0.,)*mesh.geometric_dimension())
+    outdir_path.mkdir(parents = True, exist_ok = True) 
     
-    sapphire.mms.verify_temporal_order_of_accuracy(
-        sim_module = sim_module,
-        manufactured_solution = manufactured_solution,
-        sim_constructor_kwargs = sim_constructor_kwargs,
-        mesh = mesh,
-        parameters = parameters,
-        expected_order = 3,
-        tolerance = tolerance,
-        timestep_sizes = timestep_sizes,
-        endtime = endtime,
-        outfile = open(tempdir.join("{}/rt3_rx{}_h{}.csv".format(
-            outdir, rx, h))))
+    with open(outdir_path / "convergence.csv", "w") as outfile:
         
+        sapphire.mms.verify_spatial_order_of_accuracy(
+            sim_module = sim_module,
+            sim_constructor_kwargs = sim_constructor_kwargs,
+            manufactured_solution = space_verification_solution,
+            meshes = [fe.UnitSquareMesh(n, n) for n in mesh_sizes],
+            parameters = parameters,
+            expected_order = 2,
+            tolerance = tolerance,
+            timestep_size = endtime,
+            endtime = endtime,
+            outfile = outfile)
+            
+        
+def test__verify__second_order_temporal_convergence__via_mms(
+        tempdir,
+        parameters = {
+            "grashof_number": Gr,
+            "prandtl_number": Pr,
+            "stefan_number": Ste,
+            "density_solid_to_liquid_ratio": rhos_over_rhol,
+            "heat_capacity_solid_to_liquid_ratio": cs_over_cl,
+            "thermal_conductivity_solid_to_liquid_ratio": kappas_over_kappal,
+            "smoothing": sigma},
+        meshsize = 48,
+        timestep_sizes = (1, 1/2, 1/4, 1/8, 1/16),
+        tolerance = 0.2):
+    
+    testdir = "{}/{}/".format(
+        __name__.replace(".", "/"), sys._getframe().f_code.co_name)
+    
+    outdir_path = pathlib.Path(tempdir) / testdir
+    
+    outdir_path.mkdir(parents = True, exist_ok = True) 
+    
+    with open(outdir_path / "convergence.csv", "w") as outfile:
+    
+        sapphire.mms.verify_temporal_order_of_accuracy(
+            sim_module = sim_module,
+            manufactured_solution = time_verification_solution,
+            mesh = fe.UnitSquareMesh(meshsize, meshsize),
+            parameters = parameters,
+            expected_order = 2,
+            tolerance = tolerance,
+            timestep_sizes = timestep_sizes,
+            endtime = endtime,
+            outfile = outfile)
+
+
+def test__verify__second_order_temporal_convergence__low_quadrature__via_mms(
+        tempdir,
+        sim_constructor_kwargs = {"quadrature_degree": 2},
+        parameters = {
+            "grashof_number": Gr,
+            "prandtl_number": Pr,
+            "stefan_number": Ste,
+            "density_solid_to_liquid_ratio": rhos_over_rhol,
+            "heat_capacity_solid_to_liquid_ratio": cs_over_cl,
+            "thermal_conductivity_solid_to_liquid_ratio": kappas_over_kappal,
+            "smoothing": sigma},
+        meshsize = 48,
+        timestep_sizes = (1, 1/2, 1/4, 1/8, 1/16),
+        tolerance = 0.2):
+    
+    testdir = "{}/{}/".format(
+        __name__.replace(".", "/"), sys._getframe().f_code.co_name)
+    
+    outdir_path = pathlib.Path(tempdir) / testdir
+    
+    outdir_path.mkdir(parents = True, exist_ok = True) 
+    
+    with open(outdir_path / "convergence.csv", "w") as outfile:
+    
+        sapphire.mms.verify_temporal_order_of_accuracy(
+            sim_module = sim_module,
+            sim_constructor_kwargs = sim_constructor_kwargs,
+            manufactured_solution = time_verification_solution,
+            mesh = fe.UnitSquareMesh(meshsize, meshsize),
+            parameters = parameters,
+            expected_order = 2,
+            tolerance = tolerance,
+            timestep_sizes = timestep_sizes,
+            endtime = endtime,
+            outfile = outfile)
+            
