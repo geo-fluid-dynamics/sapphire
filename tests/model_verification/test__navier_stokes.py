bead680cb5c52bc5136600603cefd69e6ccc5e72
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2019-05-22 22:37:34 +0200

    Add default element degree and do not test reduced quadrature

--- tests/model_verification/test__navier_stokes.py
+++ tests/model_verification/test__navier_stokes.py
@@ -24,7 +24,6 @@ def test__verify_convergence_order_via_mms(
     
     sapphire.mms.verify_spatial_order_of_accuracy(
         sim_module = sim_module,
-        sim_constructor_kwargs = {"quadrature_degree": 4, "element_degree": 1},
         manufactured_solution = manufactured_solution,
         meshes = [fe.UnitSquareMesh(n, n) for n in mesh_sizes],
         expected_order = 2,
