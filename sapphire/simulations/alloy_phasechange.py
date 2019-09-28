02370c6c86c4c233b3020968afb6a330323442c4
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2019-09-28 16:44:51 +0200

    Remove plotting of h/hmax

--- sapphire/simulations/alloy_phasechange.py
+++ sapphire/simulations/alloy_phasechange.py
@@ -158,14 +158,10 @@ def plotvars(sim, solution = None):
     phil = fe.interpolate(liquid_volume_fraction(
         sim = sim, enthalpy = h, solute_concentration = S), V)
     
-    h_over_hmax = fe.Function(h)
-    
-    h_over_hmax.vector()[:] = h.vector().array()/h.vector().array().max()
-    
-    return (h, S, phil, h_over_hmax), \
-        ("h", "S", "\\phi_l", "h/h_{max}"), \
-        ("h", "S", "phil", "hoverhmax")
-        
+    return (h, S, phil), \
+        ("h", "S", "\\phi_l"), \
+        ("h", "S", "phil")
+     
     
 class Simulation(sapphire.simulation.Simulation):
     
