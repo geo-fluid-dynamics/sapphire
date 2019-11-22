0582b2b576eee808041e95ab0c83deb024c6557b
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2019-11-22 13:34:01 +0100

    Report the mush/liquid interface position

--- sapphire/benchmarks/diffusive_solidification_of_alloy.py
+++ sapphire/benchmarks/diffusive_solidification_of_alloy.py
@@ -42,6 +42,38 @@ def dirichlet_boundary_conditions(sim):
         fe.DirichletBC(W.sub(0), h_c, 1),
         fe.DirichletBC(W.sub(1), S_lc, 1)]
     
+    
+def find_mush_liquid_interface_position(positions, porosities, interface_porosity):
+        
+        x = positions
+        
+        phi_l = porosities
+        
+        phi_ml = interface_porosity
+        
+        x_1 = x[-1]
+        
+        phi_l1 = phi_l[-1]
+        
+        assert(phi_l1 > interface_porosity)
+        
+        for x_0, phi_l0 in zip(reversed(x[:-1]), reversed(phi_l[:-1])):
+            
+            if phi_l0 < interface_porosity:  
+                # then interface is within the cell [x_0, x_1],
+                # so use linear interpolation to find the 
+                # interface position defined by the interface porosity.
+                
+                x_ml =  x_0 + (phi_ml - phi_l0)*(x_1 - x_0)/(phi_l1 - phi_l0)
+                
+                return x_ml
+                
+            x_1 = x_0
+            
+            phi_l1 = phi_l0
+            
+        return 0.
+    
 
 class Simulation(sapphire.simulations.alloy_phasechange.Simulation):
     
@@ -52,16 +84,20 @@ class Simulation(sapphire.simulations.alloy_phasechange.Simulation):
             mesh_cellcount, 
             cutoff_length,
             initial_porosity = 1.,
+            mush_liquid_interface_porosity = 1. - 1.e-3,
             **kwargs):
         
         self.initial_liquid_solute_concentration = fe.Constant(
             initial_liquid_solute_concentration)
         
         self.initial_porosity = fe.Constant(initial_porosity)
-            
-        self.cold_boundary_temperature = fe.Constant(cold_boundary_temperature)
+        
+        self.cold_boundary_temperature = fe.Constant(
+            cold_boundary_temperature)
         
         self.cold_boundary_porosity = fe.Constant(cold_boundary_porosity)
+        
+        self.mush_liquid_interface_porosity = mush_liquid_interface_porosity
         
         super().__init__(
             *args,
@@ -70,3 +106,13 @@ class Simulation(sapphire.simulations.alloy_phasechange.Simulation):
             dirichlet_boundary_conditions = dirichlet_boundary_conditions,
             **kwargs)
         
+    def postprocess(self):
+        
+        super().postprocess()
+        
+        self.mush_liquid_interface_position = find_mush_liquid_interface_position(
+            positions = self.mesh.coordinates.vector().array(),
+            porosities = self.postprocessed_regularized_porosity.vector().array(),
+            interface_porosity = self.mush_liquid_interface_porosity)
+        
+        return self
