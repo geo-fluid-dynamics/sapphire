29c7396f8df960ba110d00441263a621bce9cd13
alexanderzimmerman <zimmerman@aices.rwth-aachen.de>
2020-03-27 16:19:14 +0100

    Add Reynolds number parameter to Navier-Stokes simulation and validate the Re = 100 case from ghia1982

--- sapphire/simulations/navier_stokes.py
+++ sapphire/simulations/navier_stokes.py
@@ -12,10 +12,12 @@ def weak_form_residual(sim, solution):
     
     psi_u, psi_p = fe.TestFunctions(solution.function_space())
     
+    Re = sim.reynolds_number
+    
     mass = psi_p*div(u)
     
     momentum = dot(psi_u, grad(u)*u) - div(psi_u)*p + \
-        2.*inner(sym(grad(psi_u)), sym(grad(u)))
+        2./Re*inner(sym(grad(psi_u)), sym(grad(u)))
     
     dx = fe.dx(degree = sim.quadrature_degree)
     
@@ -25,15 +27,17 @@ def weak_form_residual(sim, solution):
 def element(cell, degree):
 
     return fe.MixedElement(
-        fe.VectorElement("P", cell, degree + 1),
-        fe.FiniteElement("P", cell, degree))
+        fe.VectorElement("P", cell, degree[0]),
+        fe.FiniteElement("P", cell, degree[1]))
         
         
 def strong_residual(sim, solution):
     
     u, p = solution
     
-    r_u = grad(u)*u + grad(p) - 2.*div(sym(grad(u)))
+    Re = sim.reynolds_number
+    
+    r_u = grad(u)*u + grad(p) - 2./Re*div(sym(grad(u)))
     
     r_p = div(u)
     
@@ -42,7 +46,13 @@ def strong_residual(sim, solution):
     
 class Simulation(sapphire.simulation.Simulation):
     
-    def __init__(self, *args, mesh, element_degree = 1, **kwargs):
+    def __init__(self, *args, 
+            mesh,
+            element_degree = (2, 1),
+            reynolds_number = 1.,
+            **kwargs):
+        
+        self.reynolds_number = fe.Constant(reynolds_number)
         
         super().__init__(*args,
             mesh = mesh,
--- tests/model_verification/test__navier_stokes.py
+++ tests/model_verification/test__navier_stokes.py
@@ -24,9 +24,61 @@ def test__verify_convergence_order_via_mms(
     
     sapphire.mms.verify_spatial_order_of_accuracy(
         sim_module = sim_module,
+        sim_kwargs = {"reynolds_number": 3.},
         manufactured_solution = manufactured_solution,
         meshes = [fe.UnitSquareMesh(n, n) for n in mesh_sizes],
         norms = ("H1", "L2"),
         expected_orders = (2, 2),
         tolerance = tolerance)
+        
+        
+def test__lid_driven_cavity_benchmark():
+    """ Validate against steady state lid-driven cavity benchmark.
     
+    Comparing to data published in 
+    
+        @article{ghia1982high-re,
+            author = {Ghia, Urmila and Ghia, K.N and Shin, C.T},
+            year = {1982},
+            month = {12},
+            pages = {387-411},
+            title = {High-Re solutions for incompressible flow using the Navier-Stokes equations and a multigrid method1},
+            volume = {48},
+            journal = {Journal of Computational Physics},
+            doi = {10.1016/0021-9991(82)90058-4}
+        }
+    """
+    def initial_values(sim):
+
+        return sim.solution
+        
+    def dirichlet_boundary_conditions(sim):
+        
+        W = sim.function_space
+        
+        return [
+            fe.DirichletBC(W.sub(0), fe.Constant((0., 0.)), (1, 2, 3)),
+            fe.DirichletBC(W.sub(0), fe.Constant((1., 0.)), 4)]
+    
+    sim = sapphire.simulations.navier_stokes.Simulation(
+        reynolds_number = 100.,
+        initial_values = initial_values,
+        dirichlet_boundary_conditions = dirichlet_boundary_conditions,
+        mesh = fe.UnitSquareMesh(20, 20),
+        element_degree = (2, 1))
+    
+    sim.solution = sim.solve()
+    
+    sapphire.test.check_scalar_solution_component(
+        solution = sim.solution,
+        component = 0,
+        subcomponent = 0,
+        coordinates = [(0.5, y) 
+            for y in (0.9999, 0.9766, 0.1016, 0.0547, 0.0000)],
+            # Check y = 0.9999 instead of y = 1
+            # because the Function evaluation fails at the exact coordinate.
+            # See https://github.com/firedrakeproject/firedrake/issues/1340 
+        expected_values = (1.0000, 0.8412, -0.0643, -0.0372, 0.0000),
+        relative_tolerance = 1.e-2,
+        absolute_tolerance = 1.e-2)
+        
