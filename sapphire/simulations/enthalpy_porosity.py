36f624eded2b82f1f5956c5dc0c9bc563f130ec3
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2020-04-18 17:09:56 +0200

    Add local solid pressure penalty to mass equation

--- sapphire/simulations/enthalpy_porosity.py
+++ sapphire/simulations/enthalpy_porosity.py
@@ -141,13 +141,19 @@ def time_discrete_terms(sim):
 
 def mass(sim, solution):
     
-    _, u, _ = fe.split(solution)
+    p, u, T = fe.split(solution)
     
     psi_p, _, _ = fe.TestFunctions(solution.function_space())
     
+    phi_l = liquid_volume_fraction(sim = sim, temperature = T)
+    
+    phi_s = 1. - phi_l
+    
+    gamma_s = sim.solid_pressure_penalty
+    
     div = fe.div
     
-    mass = psi_p*div(u)
+    mass = psi_p*(div(u) + gamma_s*phi_s*p)
     
     return mass
     
@@ -234,6 +240,7 @@ class Simulation(sapphire.simulation.Simulation):
             heat_capacity_solid_to_liquid_ratio = 1.,
             thermal_conductivity_solid_to_liquid_ratio = 1.,
             solid_velocity_relaxation_factor = 1.e-12,
+            solid_pressure_penalty = 1.e-5,
             liquidus_smoothing_factor = 0.01,
             solver_parameters = default_solver_parameters,
             **kwargs):
@@ -257,6 +264,9 @@ class Simulation(sapphire.simulation.Simulation):
         
         self.solid_velocity_relaxation_factor = fe.Constant(
             solid_velocity_relaxation_factor)
+        
+        self.solid_pressure_penalty = fe.Constant(
+            solid_pressure_penalty)
         
         self.liquidus_smoothing_factor = fe.Constant(
             liquidus_smoothing_factor)
--- tests/regression/test__melt_octadecane.py
+++ tests/regression/test__melt_octadecane.py
@@ -13,13 +13,14 @@ def test__validate__melt_octadecane__regression(tempdir):
     solver_parameters["snes_linesearch_damping"] = 1.
     
     sim = sapphire.simulations.examples.melt_octadecane.Simulation(
-        element_degree = (1, 2, 2),
+        element_degree = (1, 2, 1),
         mesh = fe.UnitSquareMesh(24, 24),
         quadrature_degree = 4,
         time_stencil_size = 3,
-        timestep_size = 5.,
-        liquidus_smoothing_factor = 0.05,
-        solid_velocity_relaxation_factor = 1.e-10,
+        timestep_size = 20.,
+        liquidus_smoothing_factor = 0.005,
+        solid_velocity_relaxation_factor = 1.e-12,
+        solid_pressure_penalty = 1.e-4,
         solver_parameters = solver_parameters,
         output_directory_path = tempdir)
     
@@ -27,5 +28,5 @@ def test__validate__melt_octadecane__regression(tempdir):
     
     print("Liquid area = {}".format(sim.liquid_area))
     
-    assert(abs(sim.liquid_area - 0.41) < 0.01)
+    assert(abs(sim.liquid_area - 0.47) < 0.01)
     
